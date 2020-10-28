b7a7e8850143dbc9ba728f9cea006dca1a9f93e6
Samuel Niederer <samuel.niederer@ost.ch>
2020-10-28 17:19:22 +0100

    add system files

---
+++ system.h
@@ -0,0 +1,21 @@
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+
+void Error_Handler(void);
+void SystemClock_Config(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
